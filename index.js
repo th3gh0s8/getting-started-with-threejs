6afaa22a6aa508c9f3f1536675676d6d2dc8951f
Chamuditha Pasindu <chamudithapasindu54@gmail.com>
2026-05-26 10:07:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const wireMat = new THREE.MeshBasicMaterial({
     wireframe: true,
 });
 const wireMesh = new THREE.Mesh(geo, wireMat);
-scene.add(wireMesh);
+mesh.add(wireMesh);
 
 const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000);
 scene.add(hemiLight);
