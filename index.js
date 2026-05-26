5fb1fefee062df04382bebcf251be6f0706f4d64
Chamuditha Pasindu <chamudithapasindu54@gmail.com>
2026-05-26 10:05:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,12 +22,20 @@ const mat = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geo, mat);
 scene.add(mesh);
 
+const wireMat = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    wireframe: true,
+});
+const wireMesh = new THREE.Mesh(geo, wireMat);
+scene.add(wireMesh);
+
 const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000);
 scene.add(hemiLight);
 
-function animate() {
+function animate(t = 0) {
     requestAnimationFrame(animate);
     /* mesh.scale.setScalar(Math.cos(t * 0.001) + 1.0); */
+    mesh.rotation.y = t * 0.0001;
     renderer.render(scene, camera);
 }
 
